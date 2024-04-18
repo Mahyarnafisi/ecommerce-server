12d69e879a586400cd6ecf71b678da377d98829a
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-18 15:06:44 +0200

    get all the products to their respective controllers

--- controllers/switches.controller.js
+++ controllers/switches.controller.js
@@ -3,7 +3,8 @@ import Switch from "./../models/switches.model.js";
 
 export const getSwitches = async (req, res) => {
   try {
-    res.send("switches");
+    const getAllSwitches = await Switch.find();
+    res.status(200).json({ status: "success", data: getAllSwitches });
   } catch (err) {
     console.log(err, "from getSwitches");
   }
