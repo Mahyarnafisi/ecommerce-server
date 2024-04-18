3a706adedb7a70ccf546445c279d73bea15da45a
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-18 15:09:58 +0200

    get requested product through params in filter end point

--- controllers/filters.controller.js
+++ controllers/filters.controller.js
@@ -2,8 +2,10 @@ import express from "express";
 import Filter from "./../models/filters.model.js";
 
 export const getFilter = async (req, res) => {
+  const { product } = req.params;
   try {
-    res.send("filter works!");
+    const getFilterOfProduct = await Filter.find({ product: product });
+    res.status(200).json({ status: "success", data: getFilterOfProduct });
   } catch (err) {
     console.log(err, "from getFilter");
   }
