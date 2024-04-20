397c2416e9ea8120b31c9d4a0b49aa1e995c413c
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-20 18:23:48 +0200

    initiate logic in keyboards controller to provide all keyboards and first 5 keyboards base on the request query parameters

--- controllers/keyboards.controller.js
+++ controllers/keyboards.controller.js
@@ -5,6 +5,20 @@ import Keyboard from "./../models/keyboards.model.js";
 export const getKeyboard = async (req, res) => {
   try {
     const getAllKeyboards = await Keyboard.find();
+
+    if (!req.query) {
+      return res.status(200).json({ status: "Here is all keyboards data", data: getAllKeyboards });
+    }
+    if (req.query && req.query.first) {
+      const queryFirstNumber = req.query.first;
+      const sliceOfData = getAllKeyboards.slice(0, queryFirstNumber);
+      return res.status(200).json({
+        status: `Here is the first${queryFirstNumber > 1 ? " " + queryFirstNumber : ""} ${queryFirstNumber > 1 ? "keyboards" : "keyboard"}`,
+        numberOfData: sliceOfData.length,
+        data: sliceOfData,
+      });
+    }
+
     res.status(200).json({ status: "success", data: getAllKeyboards });
   } catch (err) {
     console.log(err, "from getKeyboard");
