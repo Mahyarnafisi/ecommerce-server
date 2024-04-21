38b37879c81cdb6a91510780b02d7036917ff302
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-21 12:24:13 +0200

    done with sorting the keyboards in the controller file with adding new query to the database

--- controllers/keyboards.controller.js
+++ controllers/keyboards.controller.js
@@ -17,6 +17,42 @@ export const getKeyboard = async (req, res) => {
       });
     }
 
+    // Check if there is a query and the query in sort and direction to list the data in ASC direction  ⬆️
+    if (req.query && req.query.sort === "price" && req.query.direction === "asc") {
+      // Sort the data in ASC direction
+      const ascSorting = getAllKeyboards.sort((a, b) => a.price - b.price);
+      return res.status(200).json({
+        status: "asc sorting",
+        numberOfData: ascSorting.length,
+        startingPrice: ascSorting[0].price,
+        data: ascSorting,
+      });
+    }
+
+    // Check if there is a query and the query in sort and direction to list the data in DESC direction  ⬇️
+    if (req.query && req.query.sort === "price" && req.query.direction === "desc") {
+      // Sort the data in DESC direction
+      const descSorting = getAllKeyboards.sort((a, b) => b.price - a.price);
+      return res.status(200).json({
+        status: "desc sorting",
+        numberOfData: descSorting.length,
+        startingPrice: descSorting[0].price,
+        data: descSorting,
+      });
+    }
+
+    // Check if there is a query and the query in sort and direction to list the data in POPULAR ITEM  ❤️
+    if (req.query && req.query.sort === "popular") {
+      // Sort the data in POPULAR ITEM
+      const popularSorting = getAllKeyboards.sort((a, b) => b.popularity - a.popularity);
+      return res.status(200).json({
+        status: "popularity sorting",
+        numberOfData: popularSorting.length,
+        mostPopular: popularSorting[0].popularity,
+        data: popularSorting,
+      });
+    }
+
     // Check if there is no query and return all data in keyboards collection in the database ✅
     return res.status(200).json({ status: "Here is all keyboards data", numberOfData: getAllKeyboards.length, data: getAllKeyboards });
   } catch (err) {
