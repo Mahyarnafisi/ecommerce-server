0a0f89e79e423a1f7a77fed10caf469f775cf474
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-21 13:42:42 +0200

    done ith sorting queries in accessories controller

--- controllers/accessories.controller.js
+++ controllers/accessories.controller.js
@@ -4,6 +4,7 @@ import Accessory from "../models/accessories.model.js";
 export const getAccessories = async (req, res) => {
   try {
     const getAllAccessories = await Accessory.find();
+    console.log(req.query);
 
     // Check if there is a query and the query is "first" and return the first n number of data in accessories collection in the database ✅
     if (req.query && req.query.first) {
@@ -13,6 +14,37 @@ export const getAccessories = async (req, res) => {
         status: "Here is the first" + (queryFirstNumber > 1 ? " " + queryFirstNumber : "") + (queryFirstNumber > 1 ? "accessories" : "accessory"),
         numberOfData: sliceOfData.length,
         data: sliceOfData,
+      });
+    }
+
+    // Check if there is a query and the query in sort and direction to list the data in ASC and DESC direction  ⬇️⬆️
+    if (req.query && req.query.sort === "price" && (req.query.direction === "asc" || req.query.direction === "desc")) {
+      // Sort the data in ASC and DESC direction
+      const sorting = getAllAccessories.sort((a, b) => {
+        if (req.query.direction === "asc") {
+          return a.price - b.price;
+        }
+        if (req.query.direction === "desc") {
+          return b.price - a.price;
+        }
+      });
+      return res.status(200).json({
+        status: req.query.direction === "asc" ? "asc sorting accessories" : "desc sorting accessories",
+        numberOfData: sorting.length,
+        startingPrice: sorting[0].price,
+        data: sorting,
+      });
+    }
+
+    // Check if there is a query and the query in sort to list the data in POPULAR ITEM  ❤️
+    if (req.query && req.query.sort === "popular") {
+      // Sort the data in POPULAR ITEM
+      const popularSorting = getAllAccessories.sort((a, b) => b.popularity - a.popularity);
+      return res.status(200).json({
+        status: "popularity sorting",
+        numberOfData: popularSorting.length,
+        mostPopular: popularSorting[0].popularity,
+        data: popularSorting,
       });
     }
 
