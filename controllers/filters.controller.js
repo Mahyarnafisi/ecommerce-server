1b9c914c9798f7c78695fe483ec898dcf635b03d
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-23 10:21:05 +0200

    update entire Keyboard backend and queries

--- controllers/filters.controller.js
+++ controllers/filters.controller.js
@@ -1,11 +1,28 @@
 import express from "express";
 import Filter from "./../models/filters.model.js";
+import Keyboard from "../models/keyboards.model.js";
+import Keycap from "../models/keycaps.model.js";
+import Switch from "../models/switches.model.js";
+import Accessory from "../models/accessories.model.js";
 
 export const getFilter = async (req, res) => {
   const { product } = req.params;
   try {
     const getFilterOfProduct = await Filter.find({ product: product });
-    res.status(200).json({ status: "success", data: getFilterOfProduct });
+    const getAllFiltersOptions = getFilterOfProduct[0].filters.map((filter) => {
+      return filter;
+    });
+    const array = getAllFiltersOptions.flatMap((item) => {
+      return item.options;
+    });
+
+    console.log(array);
+    const test = {
+      [array.filter((item) => {
+        return { [item]: 1 };
+      })]: array,
+    };
+    return res.status(200).json({ status: "success", data: test });
   } catch (err) {
     console.log(err, "from getFilter");
   }
