91ba6202d411357054be458038a0db1caa55d011
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-27 16:40:09 +0200

    update filters.controller.js

--- controllers/filters.controller.js
+++ controllers/filters.controller.js
@@ -9,20 +9,20 @@ export const getFilter = async (req, res) => {
   const { product } = req.params;
   try {
     const getFilterOfProduct = await Filter.find({ product: product });
-    const getAllFiltersOptions = getFilterOfProduct[0].filters.map((filter) => {
-      return filter;
-    });
-    const array = getAllFiltersOptions.flatMap((item) => {
-      return item.options;
-    });
+    // const getAllFiltersOptions = getFilterOfProduct[0].filters.map((filter) => {
+    //   return filter;
+    // });
+    // const array = getAllFiltersOptions.flatMap((item) => {
+    //   return item.options;
+    // });
 
-    console.log(array);
-    const test = {
-      [array.filter((item) => {
-        return { [item]: 1 };
-      })]: array,
-    };
-    return res.status(200).json({ status: "success", data: test });
+    // console.log(array);
+    // const test = {
+    //   [array.filter((item) => {
+    //     return { [item]: 1 };
+    //   })]: array,
+    // };
+    return res.status(200).json({ status: "success", data: getFilterOfProduct });
   } catch (err) {
     console.log(err, "from getFilter");
   }
