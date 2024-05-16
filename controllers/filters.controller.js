452fd9a1ed85ae135a1a0771712e2c59cdfa849e
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-05-16 16:34:42 +0200

    update the filter panel data and logic for the filter panel

--- controllers/filters.controller.js
+++ controllers/filters.controller.js
@@ -7,8 +7,83 @@ import Accessory from "../models/accessories.model.js";
 
 export const getFilter = async (req, res) => {
   const { product } = req.params;
+
   try {
     const getFilterOfProduct = await Filter.find({ product: product });
+    if (product === "keyboards") {
+      const getNumberOfProductTypeFilter = [
+        {
+          title: "connectivity",
+          options: [
+            {
+              title: "Tri-mode wireless",
+              number: await Keyboard.find({ connectivity: "Tri-mode wireless" }).countDocuments(),
+            },
+            {
+              title: "wired",
+              number: await Keyboard.find({ connectivity: "wired" }).countDocuments(),
+            },
+          ],
+        },
+        {
+          title: "profile_type",
+          options: [
+            {
+              title: "low-profile",
+              number: await Keyboard.find({ profile_type: "low-profile" }).countDocuments(),
+            },
+            {
+              title: "normal-profile",
+              number: await Keyboard.find({ profile_type: "normal-profile" }).countDocuments(),
+            },
+          ],
+        },
+        {
+          title: "series",
+          options: [
+            {
+              title: "gem",
+              number: await Keyboard.find({ series: "gem" }).countDocuments(),
+            },
+            {
+              title: "air",
+              number: await Keyboard.find({ series: "air" }).countDocuments(),
+            },
+            {
+              title: "halo",
+              number: await Keyboard.find({ series: "halo" }).countDocuments(),
+            },
+            {
+              title: "field",
+              number: await Keyboard.find({ series: "field" }).countDocuments(),
+            },
+          ],
+        },
+        {
+          title: "size",
+          options: [
+            {
+              title: "60",
+              number: await Keyboard.find({ size: "60" }).countDocuments(),
+            },
+            {
+              title: "65",
+              number: await Keyboard.find({ size: "65" }).countDocuments(),
+            },
+            {
+              title: "75",
+              number: await Keyboard.find({ size: "75" }).countDocuments(),
+            },
+            {
+              title: "96",
+              number: await Keyboard.find({ size: "96" }).countDocuments(),
+            },
+          ],
+        },
+      ];
+
+      return res.status(200).json({ status: "success", data: getNumberOfProductTypeFilter });
+    }
     // const getAllFiltersOptions = getFilterOfProduct[0].filters.map((filter) => {
     //   return filter;
     // });
@@ -22,7 +97,6 @@ export const getFilter = async (req, res) => {
     //     return { [item]: 1 };
     //   })]: array,
     // };
-    return res.status(200).json({ status: "success", data: getFilterOfProduct });
   } catch (err) {
     console.log(err, "from getFilter");
   }
--- controllers/keyboards.controller.js
+++ controllers/keyboards.controller.js
@@ -3,28 +3,49 @@ const app = express();
 import Keyboard from "./../models/keyboards.model.js";
 
 export const getKeyboard = async (req, res) => {
+  console.log(req.query, "from getKeyboard");
   try {
     // find all data in keyboards collection in the database
     if (!req.query.filter && !req.query.sort && !req.query.first) {
       const getAllKeyboards = await Keyboard.find();
-      return res.status(200).json({ status: "no filter", numberOfData: getAllKeyboards.length, data: getAllKeyboards });
+      return res
+        .status(200)
+        .json({ status: "no filter", numberOfData: getAllKeyboards.length, data: getAllKeyboards });
     }
     // find based on queries when sort in POPULAR
     if (req.query.sort === "popular" && !req.query.direction && !req.query.filter) {
       const getAllKeyboards = await Keyboard.find().sort({ popularity: -1 });
-      return res.status(200).json({ status: "most popular items", numberOfData: getAllKeyboards.length, data: getAllKeyboards });
+      return res
+        .status(200)
+        .json({
+          status: "most popular items",
+          numberOfData: getAllKeyboards.length,
+          data: getAllKeyboards,
+        });
     }
 
     // find based on queries when sort in PRICE and DIRECTION in ASCENDING or DESCENDING
     if (req.query.sort && req.query.direction && !req.query.filter) {
       const getAllKeyboards = await Keyboard.find().sort({ [req.query.sort]: req.query.direction });
-      return res.status(200).json({ status: `${req.query.direction}`, numberOfData: getAllKeyboards.length, data: getAllKeyboards });
+      return res
+        .status(200)
+        .json({
+          status: `${req.query.direction}`,
+          numberOfData: getAllKeyboards.length,
+          data: getAllKeyboards,
+        });
     }
 
     // find the first n number of data in keyboards collection in the database
     if (req.query.first) {
       const getAllKeyboards = await Keyboard.find().limit(parseInt(req.query.first));
-      return res.status(200).json({ status: "successful", numberOfData: getAllKeyboards.length, data: getAllKeyboards });
+      return res
+        .status(200)
+        .json({
+          status: "successful",
+          numberOfData: getAllKeyboards.length,
+          data: getAllKeyboards,
+        });
     }
 
     // find based on queries in filter and sort and direction and popular
@@ -46,9 +67,9 @@ export const getKeyboard = async (req, res) => {
 
       // find based on queries in filter and sort and direction and popular if THERE IS NO ARRAY
       if (typeof req.query.filterProperty === "string") {
-        const getAllKeyboards = await Keyboard.find({ [req.query.filterProperty]: req.query.value }).sort(
-          req.query.direction ? { price: req.query.direction } : { popularity: -1 }
-        );
+        const getAllKeyboards = await Keyboard.find({
+          [req.query.filterProperty]: req.query.value,
+        }).sort(req.query.direction ? { price: req.query.direction } : { popularity: -1 });
 
         return res.status(200).json({
           status: getAllKeyboards.length === 0 ? "There is no result found!" : "success",
