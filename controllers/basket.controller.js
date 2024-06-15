8a0d7b5f78edb4b78b6fff1e045f4729972982f8
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-06-15 22:19:13 +0200

    done with basket logic -read all data from basket for a online user -add item to basket -remove item from basket -increment item quantity -decrement item quantity

--- controllers/basket.controller.js
+++ controllers/basket.controller.js
@@ -3,15 +3,17 @@ import Basket from "../models/basket.model.js";
 export const getBasket = async (req, res) => {
   const { userID } = req.params;
   try {
+    const userBasketData = await Basket.findOne({ userID: userID });
     res.status(200).json({
       message: "GET Basket",
+      data: userBasketData.basketList.length !== 0 ? userBasketData?.basketList : "There is no item in basket list",
     });
   } catch (error) {
     console.log(error, "GET Basket");
   }
 };
 
-export const postBasket = async (req, res) => {
+export const addBasketItem = async (req, res) => {
   const { userID } = req.params;
   const getUser = await Basket.findOne({ userID: userID });
 
@@ -19,7 +21,6 @@ export const postBasket = async (req, res) => {
     // If the user already has a basket list, update it
     if (getUser) {
       const item = getUser.basketList?.find((item) => item.itemID === req.body.itemObject.itemID);
-
       // If the item already exists in the basket list, update the quantity +1
       if (item) {
         item.quantity += 1;
@@ -36,13 +37,6 @@ export const postBasket = async (req, res) => {
           data: getUser.basketList,
         });
       }
-
-      // Update the user's basket list
-      // await Basket.findOneAndUpdate({ userID: userID }, { $push: { basketList: req.body.itemObject } }, { new: true });
-
-      return res.status(200).json({
-        message: "POST Basket",
-      });
     }
 
     // If the user does not have a basket list, create a new one
@@ -62,8 +56,10 @@ export const postBasket = async (req, res) => {
   }
 };
 
-export const deleteBasket = async (req, res) => {
+//  Delete the item from the user's basket list
+export const deleteBasketItem = async (req, res) => {
   const { userID } = req.params;
+
   try {
     // Delete the item from the user's basket list
     await Basket.findOneAndUpdate({
@@ -71,9 +67,91 @@ export const deleteBasket = async (req, res) => {
       $pull: { basketList: { itemID: req.body.itemID } },
     });
     return res.status(200).json({
-      message: "DELETE Basket",
+      message: "DELETE item from  Basket",
     });
   } catch (error) {
     console.log(error, "DELETE Basket");
+  }
+};
+
+//
+export const incrementBasketItem = async (req, res) => {
+  const { userID } = req.params;
+  const getUser = await Basket.findOne({ userID: userID });
+
+  try {
+    if (getUser) {
+      const item = getUser.basketList?.find((item) => item.itemID === req.body.itemObject.itemID);
+
+      if (item) {
+        item.quantity += 1;
+        await Basket.findOneAndUpdate({ userID: userID }, { $set: { basketList: getUser.basketList } });
+        return res.status(200).json({
+          data: getUser.basketList,
+        });
+      }
+
+      if (!item) {
+        await Basket.findOneAndUpdate({ userID: userID }, { $push: { basketList: { ...req.body.itemObject, quantity: 1 } } });
+        return res.status(200).json({
+          data: getUser.basketList,
+        });
+      }
+    }
+
+    if (!getUser) {
+      // Create a new basket list
+      const newBasket = new Basket({
+        userID: userID,
+        basketList: { ...req.body.itemObject, quantity: 1 },
+      });
+      await newBasket.save();
+      return res.status(200).json({
+        message: "POST Basket",
+      });
+    }
+  } catch (error) {
+    console.log(error, "INCREMENT ITEM Basket");
+  }
+};
+
+//
+export const decrementBasketItem = async (req, res) => {
+  const { userID } = req.params;
+  const getUser = await Basket.findOne({ userID: userID });
+
+  try {
+    if (getUser) {
+      const item = getUser.basketList?.find((item) => item.itemID === req.body.itemObject.itemID);
+
+      if (item) {
+        item.quantity -= 1;
+        await Basket.findOneAndUpdate({ userID: userID }, { $set: { basketList: getUser.basketList } });
+        return res.status(200).json({
+          data: getUser.basketList,
+        });
+      }
+
+      if (!item) {
+        await Basket.findOneAndUpdate({ userID: userID }, { $push: { basketList: { ...req.body.itemObject, quantity: 1 } } });
+        return res.status(200).json({
+          data: getUser.basketList,
+        });
+      }
+    }
+
+    if (!getUser) {
+      // Create a new basket list
+      const newBasket = new Basket({
+        userID: userID,
+        basketList: { ...req.body.itemObject, quantity: 1 },
+      });
+      await newBasket.save();
+      return res.status(200).json({
+        message: "POST Basket",
+      });
+    }
+  } catch (error) {
+    console.log(error, "INCREMENT ITEM Basket");
   }
 };
--- routers/basket.routes.js
+++ routers/basket.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getBasket, postBasket, deleteBasket } from "./../controllers/basket.controller.js";
+import { getBasket, addBasketItem, deleteBasketItem, decrementBasketItem, incrementBasketItem } from "./../controllers/basket.controller.js";
 
 const router = express.Router();
 
 router.get("/:userID", getBasket);
-router.post("/:userID", postBasket);
-router.delete("/:userID", deleteBasket);
+router.post("/add/:userID", addBasketItem);
+router.post("/increment/:userID", incrementBasketItem);
+router.post("/decrement/:userID", decrementBasketItem);
+router.delete("/del/:userID", deleteBasketItem);
 
 export default router;
