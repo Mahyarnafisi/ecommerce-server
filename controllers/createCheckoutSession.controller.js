a2e812440be7b33af9bc003a5c7204f0483be8b3
Mahyar Nafisi <mahyar.nafisi@gmail.com>
2024-07-31 15:32:15 +0200

    implement stripe route and check out

---
+++ controllers/createCheckoutSession.controller.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import stripe from "stripe";
+const stripeAuth = stripe(process.env.SECRET_STRIPE_KEY);
+
+export const createCheckoutSession = async (req, res) => {
+  const products = req.body;
+  const lineItems = products.map((product) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: product.title,
+        },
+        unit_amount: Math.round(product.price * 100),
+      },
+      quantity: product.quantity,
+    };
+  });
+
+  const session = await stripeAuth.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:5173/wishlist",
+    cancel_url: "http://localhost:5173/checkout",
+  });
+
+  res.json({ id: session.id });
+};
--- routers/basket.routes.js
+++ routers/basket.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { getBasket, addBasketItem, deleteBasketItem, decrementBasketItem, incrementBasketItem } from "./../controllers/basket.controller.js";
+import {
+  getBasket,
+  addBasketItem,
+  deleteBasketItem,
+  decrementBasketItem,
+  incrementBasketItem,
+} from "./../controllers/basket.controller.js";
+import { createCheckoutSession } from "../controllers/createCheckoutSession.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +15,8 @@ router.post("/add/:userID", addBasketItem);
 router.post("/increment/:userID", incrementBasketItem);
 router.post("/decrement/:userID", decrementBasketItem);
 router.delete("/del/:userID", deleteBasketItem);
+
+// Stripe end point / new route to create strip new checkout session
+router.post("/create-checkout-session", createCheckoutSession);
 
 export default router;
